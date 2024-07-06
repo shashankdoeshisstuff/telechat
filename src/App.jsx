27211b1c623bf754260802dfe15edd80ea83db9f
Shashank Maurya <shashankdoeshisstuff@gmail.com>
2024-07-07 00:06:18 +0530

    intialize

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* Dashboard */}
+      <main>
+        {/* Chats */}
+        <section>
+          <ul>
+            <li>chat</li>
+            <li>chat</li>
+            <li>chat</li>
+            <li>chat</li>
+            <li>chat</li>
+          </ul>
+        </section>
+        {/* Chat */}
+        <section>
+          
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
